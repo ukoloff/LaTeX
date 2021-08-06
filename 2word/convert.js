e51d35ba0813719427e92d8c74f573f7cf57554a
Stas Ukolov <ukoloff@gmail.com>
2021-08-06 12:24:44 +0300

    Make converter run

--- 2word/convert.js
+++ 2word/convert.js
@@ -1,18 +1,31 @@
 setTimeout(init)
 
 function init() {
+  'use strict';
+
   var [src, dst] = document.forms[0].getElementsByTagName('textarea')
+  var [res] = document.forms[0].getElementsByTagName('div')
 
   src.focus()
   src.oninput = update
   setTimeout(update, 112)
 
+  document.forms[0].getElementsByTagName('button')[0].onclick = selectAll
 
   var prev
   function update() {
     if (prev == src.value) return
     prev = src.value
 
-    dst.value = prev
+    katex.render(prev, res, {
+      displayMode: true,
+      throwOnError: false,
+    })
+    var [math] = res.getElementsByTagName('math')
+    dst.value = math ? math.outerHTML : ''
+  }
+
+  function selectAll() {
+    dst.select()
   }
 }
